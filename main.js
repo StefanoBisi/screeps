2e02efd623ce264e5766e21c064faa9750108c77
StefanoBisi <stefano.bisi@aol.com>
2021-01-13 21:46:49 +0100

    Controllo di sicurezza per estinzione in stanza

--- main.js
+++ main.js
@@ -34,27 +34,34 @@ module.exports.loop = function ()
 	
 	if((Game.time % 11) == 0)
 	{
-		for(room_name in Game.rooms)
+		room_loop: for(room_name in Game.rooms)
 		{
 			try
 			{
 				let room = Game.rooms[room_name];
 				if(room.controller.my && Memory.rooms[room_name] && Memory.rooms[room_name].auto)
 				{
-					for(role_name in roles)
+					let creeps_tot = 0;
+					let spawn_name = Memory.rooms[room.name].default.spawn;
+					role_loop: for(role_name in roles)
 					{
 						let n = _.sum(room.find(FIND_CREEPS), (c) => c.memory.role == role_name);
 						Memory.rooms[room_name].roles[role_name].count = n;
 						let req = Memory.rooms[room.name].roles[role_name].required;
-						let spawn_name = Memory.rooms[room.name].default.spawn;
 						if(n < req && !Game.spawns[spawn_name].spawning)
 						{
 							let role = roles[role_name];
 							let lvl =  Memory.rooms[room.name].roles[role_name].body.lvl;
 							let r = role.generate(spawn_name, lvl);
-							if(!(r < 0)) { console.log(room_name + ' - Generating ' + role_name + ' creep'); }
+							if(!(r < 0))
+							{
+								console.log(room_name + ' - Generating ' + role_name + ' creep');
+								break role_loop;
+							}
 						}
+						creeps_tot += n;
 					}
+					if(creeps_tot == 0) { roles.worker.generate(spawn_name, 0); }
 				}
 			}
 			catch(err) { console.log('ERROR - room ' + room_name + ':\n' + err.stack); }
